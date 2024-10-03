b651dcf57fdbc7894eace35e49546302c04aa566
joelcolombo <joelcolombo@gmail.com>
2024-10-03 16:26:43 -0300

    Add SW2024 Redirect

---
+++ app/sw2024/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function SelectedWorksPage() {
+  redirect('https://www.figma.com/proto/0OOliERdgcD2N6gW8bBzrY/Joel-Colombo-Selected-Works-2024?page-id=0%3A1&node-id=4-193&viewport=3457%2C1296%2C0.5&t=fMzwfl9zwUDwN0AJ-8&scaling=contain&content-scaling=fixed&starting-point-node-id=4%3A193&hotspot-hints=0&hide-ui=1');
+}
