32b0e0584d6723bb54b9921da6cdf256dd2d3bcf
joelcolombo <joelcolombo@gmail.com>
2025-02-26 11:37:34 -0300

    Updated sw2025 page metadata and open graph.

--- app/sw2025/page.tsx
+++ app/sw2025/page.tsx
@@ -4,8 +4,8 @@ import { Metadata } from 'next';
 export const generateMetadata = async (): Promise<Metadata> => {
   // You can fetch data here if needed
   return {
-    title: 'Joel Colombo ✦ Selected Works 2024',
-    description: 'Joel Colombo ✦ Selected Works 2024',
+    title: 'Joel Colombo ✦ Selected Works 2025',
+    description: 'Joel Colombo ✦ Selected Works 2025',
     icons: {
       icon: '/images/logo.png',
     },
@@ -14,7 +14,7 @@ export const generateMetadata = async (): Promise<Metadata> => {
       description: 'Presentation with projects that I worked on during the last couple of years',
       images: [
         {
-          url: '/images/og-joelcolombo-sw2024.png',
+          url: '/images/og-joelcolombo-sw2025.png',
           width: 1200,
           height: 630,
         },
@@ -27,5 +27,5 @@ export const generateMetadata = async (): Promise<Metadata> => {
 
 
 export default function SelectedWorksPage() {
-  redirect('https://www.figma.com/proto/0OOliERdgcD2N6gW8bBzrY/Joel-Colombo-Selected-Works-2024?page-id=0%3A1&node-id=4-193&viewport=3457%2C1296%2C0.5&t=fMzwfl9zwUDwN0AJ-8&scaling=contain&content-scaling=fixed&starting-point-node-id=4%3A193&hotspot-hints=0&hide-ui=1');
+  redirect('https://www.figma.com/proto/0OOliERdgcD2N6gW8bBzrY/Joel-Colombo-Selected-Works-2025?page-id=0%3A1&node-id=4-193&viewport=3457%2C1296%2C0.5&t=fMzwfl9zwUDwN0AJ-8&scaling=contain&content-scaling=fixed&starting-point-node-id=4%3A193&hotspot-hints=0&hide-ui=1');
 }
